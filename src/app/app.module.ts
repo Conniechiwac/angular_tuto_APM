5a978ac0c7c25848d518826463d0c209f5f03c5a
constance charpentier <constancecharpentier@yahoo.fr>
2020-05-04 21:16:09 +0200

    fix after update with the parallel project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-import { ProductListComponent } from './product/product-list.component';
+import { ProductListComponent } from './products/product-list.component';
 import { AppComponent } from './app.component';
 import { convertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 
